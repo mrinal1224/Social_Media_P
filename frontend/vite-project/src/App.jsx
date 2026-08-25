33f9000555ed82443c43bde92e999a1d0a958ea5
mrinal1224 <iammrinalkc@gmail.com>
2026-08-25 07:25:04 +0530

    first commit

---
+++ frontend/vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
