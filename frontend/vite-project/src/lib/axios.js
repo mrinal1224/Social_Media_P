08f62d8846f61b5d0b884948d04f959a3bd418ef
Mrinal Bhattacharya <iammrinalkc@gmail.com>
2026-09-10 10:58:25 +0530

    fix: allow browser to set multipart content type

--- frontend/vite-project/src/lib/axios.js
+++ frontend/vite-project/src/lib/axios.js
@@ -3,9 +3,6 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: "http://localhost:8083",
   withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
 export default axiosInstance;
